cdbe30d7146decafce25a495927dcf17ecf0cc03
FuyuFacu <fuyuestudios@gmail.com>
2024-10-20 23:21:25 -0300

    Add files via upload

---
+++ Ejercicio5/Cuadrado.java
@@ -0,0 +1,27 @@
+package ar.edu.unlp.info.oo1.ejercicio5;
+
+public class Cuadrado implements Figura{
+	private int lado;
+	
+	
+	public Cuadrado() {}
+	
+	
+	public void setLado(int lado) {
+		this.lado = lado;
+	}
+	
+	public double getLado() {
+		return this.lado;
+	}
+	
+	public double getPerimetro() {
+		return lado * 4;
+	}
+	
+	public double getArea() {
+		return lado * lado;
+	}
+	
+	
+}
