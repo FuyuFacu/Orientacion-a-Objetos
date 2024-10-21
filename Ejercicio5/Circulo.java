3efe95dd38f16fe4deca558b5d178c3cee43d891
FuyuFacu <fuyuestudios@gmail.com>
2024-10-20 23:20:45 -0300

    Create Circulo.java
    Cuando hacia getPerimetro() me saltaba el puto failure de mierda. Y todo era porque el puto perimetro que cuando hacia math.PI * this.diametro me tiraba failure, ya que almacenaba mal el diametro.
    Segun chatgpt era por que se desincronizaba el diametro con el radio, sigo sin entender por que pero gueno. Al final he sacado el setDiametro y me he quedado solamente con el getDiametro utilizando this.radio * 2 y a chuparla

---
+++ Ejercicio5/Circulo.java
@@ -0,0 +1,46 @@
+package ar.edu.unlp.info.oo1.ejercicio5;
+
+public class Circulo  implements Figura{
+	private double radio;
+	// private double Diametro; segun chatgpt si directamento
+	// calculo el diametro haciendo this.radio * 2 el diametro
+	// no se desincronizaria??. Gracias a eso me anduvo el programa....
+	// ya que me tiraba un failure en cilindro.getSuperficieExterior
+	// por culpa del maldito diametro
+	
+	
+	public Circulo() {}
+	
+	
+	
+	public void setRadio(double radio) {
+		this.radio = radio;
+	}
+	
+	//public void setDiametro() {
+	//	this.Diametro = this.radio * 2;
+	//}
+	
+	public double getRadio() {
+		return this.radio;
+	}
+	
+	public double getDiametro() {
+		return this.radio * 2;
+	}
+	
+	public double getPerimetro() {
+		return Math.PI * this.radio * 2;
+	}
+	
+	public double getArea() {
+		return Math.PI * Math.pow(radio, 2);
+	}
+	
+	
+	
+	
+	
+	
+	
+}
