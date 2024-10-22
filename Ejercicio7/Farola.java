1491933438febe22254f140dcecf7a7e699b2825
FuyuFacu <fuyuestudios@gmail.com>
2024-10-22 18:08:35 -0300

    Add files via upload

---
+++ Ejercicio7/Farola.java
@@ -0,0 +1,43 @@
+package ar.edu.unlp.info.oo1.ejercicio7;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Farola {
+	private boolean interruptor;
+	private List<Farola> conexiones;
+	
+	public Farola() {
+		this.interruptor = false;
+		this.conexiones = new ArrayList<>(); 
+	}
+	
+	public void pairWithNeighbor(Farola F) {
+		conexiones.add(F);
+		F.conexiones.add(this); // se agrega la reciprocidad
+	}
+	
+	
+	public List<Farola> getNeighbors() {
+		return conexiones;
+	}
+	
+	public void turnOff() {
+		if (isOn()) {
+			interruptor = false;
+			conexiones.stream().forEach(Farola::turnOff);
+		}
+	};
+	// Enciendo la actual, filtro , y en cada objeto implemento la funcion de manera recursiva, para que recorra 
+	// sus vecinos dicha farola
+	public void turnOn() {
+		if (isOff()) {
+			interruptor = true;
+			conexiones.stream().forEach(Farola::turnOn);
+		}
+	} 
+	// Apago el actual, filtro , y en cada objeto implemento la funcion de manera recursiva, para que recorra sus vecinos dicha farola
+	
+	public boolean isOn() {return this.interruptor;}  // chequeo si esta encendido, me retorna true si es verdadero
+	public boolean isOff() {return !this.interruptor;} // chequeo si esta apagado, me retorna true si es verdadero
+	
+}
